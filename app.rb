6afb448ce0d3f45ec3a8b6a867c11887ffd75c2d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-21 19:44:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
   # Code your final two routes here:
   get '/goodbye/:name' do
     @name = params[:name]
-    "goodbye #{@name}"
+    "Goodbye, #{@name}."
     #erb :goodbye.html
   end
 end
